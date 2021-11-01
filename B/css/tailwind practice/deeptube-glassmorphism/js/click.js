dadac7445179e9b71689319236d7ce2c58ea7ff3
Harsh8055 <harshsingh.ytbusiness@gmail.com>
2021-11-02 03:47:35 +0530

    mistakes corrected

--- B/css/tailwind practice/deeptube-glassmorphism/js/click.js
+++ B/css/tailwind practice/deeptube-glassmorphism/js/click.js
@@ -11,388 +11,6 @@ function webdevpage() {
     let html = ` 
    
     <div class="flex glass flex-wrap -m-4 my-2" id="webdevcontainer">
-    <div class="mx-10 my-4 glass md:w-1/4" id="frontend">
-      <div
-        class="
-          h-full
-          border-2 border-gray-200 border-opacity-60
-          rounded-lg
-          overflow-hidden
-        "
-      >
-        <img
-          class="lg:h-48 md:h-36 w-full object-cover object-center"
-          src="img/website.svg"
-          alt="blog"
-        />
-        <div class="p-6">
-          <h2
-            class="
-              tracking-widest
-              text-xs
-              title-font
-              font-medium
-              text-gray-400
-              mb-1
-            "
-          >
-            CATEGORY
-          </h2>
-          <h1
-            class="
-              title-font
-              text-lg
-              font-medium
-              text-gray-900
-              mb-3
-            "
-          >
-            The Catalyzer
-          </h1>
-          <p class="leading-relaxed mb-3">
-            Photo booth fam kinfolk cold-pressed sriracha leggings
-            jianbing microdosing tousled waistcoat.
-          </p>
-          <div class="flex items-center flex-wrap">
-            <a
-              class="
-                text-indigo-500
-                inline-flex
-                items-center
-                md:mb-2
-                lg:mb-0
-              "
-              >Learn More
-              <svg
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-            <span
-              class="
-                text-gray-400
-                mr-3
-                inline-flex
-                items-center
-                lg:ml-auto
-                md:ml-0
-                ml-auto
-                leading-none
-                text-sm
-                pr-3
-                py-1
-                border-r-2 border-gray-200
-              "
-            >
-              <svg
-                class="w-4 h-4 mr-1"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
-                ></path>
-                <circle cx="12" cy="12" r="3"></circle></svg
-              >1.2K
-            </span>
-            <span
-              class="
-                text-gray-400
-                inline-flex
-                items-center
-                leading-none
-                text-sm
-              "
-            >
-              <svg
-                class="w-4 h-4 mr-1"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                ></path></svg
-              >6
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="backend" class="mx-10 my-4 glass md:w-1/4">
-      <div
-        class="
-          h-full
-          border-2 border-gray-200 border-opacity-60
-          rounded-lg
-          overflow-hidden
-        "
-      >
-        <img
-          class="lg:h-48 md:h-36 w-full object-cover object-center"
-          src="img/website.svg"
-          alt="blog"
-        />
-        <div class="p-6">
-          <h2
-            class="
-              tracking-widest
-              text-xs
-              title-font
-              font-medium
-              text-gray-400
-              mb-1
-            "
-          >
-            CATEGORY
-          </h2>
-          <h1
-            class="
-              title-font
-              text-lg
-              font-medium
-              text-gray-900
-              mb-3
-            "
-          >
-            The 400 Blows
-          </h1>
-          <p class="leading-relaxed mb-3">
-            Photo booth fam kinfolk cold-pressed sriracha leggings
-            jianbing microdosing tousled waistcoat.
-          </p>
-          <div class="flex items-center flex-wrap">
-            <a
-              class="
-                text-indigo-500
-                inline-flex
-                items-center
-                md:mb-2
-                lg:mb-0
-              "
-              >Learn More
-              <svg
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-            <span
-              class="
-                text-gray-400
-                mr-3
-                inline-flex
-                items-center
-                lg:ml-auto
-                md:ml-0
-                ml-auto
-                leading-none
-                text-sm
-                pr-3
-                py-1
-                border-r-2 border-gray-200
-              "
-            >
-              <svg
-                class="w-4 h-4 mr-1"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
-                ></path>
-                <circle cx="12" cy="12" r="3"></circle></svg
-              >1.2K
-            </span>
-            <span
-              class="
-                text-gray-400
-                inline-flex
-                items-center
-                leading-none
-                text-sm
-              "
-            >
-              <svg
-                class="w-4 h-4 mr-1"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                ></path></svg
-              >6
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-      <h1
-        class="
-          md:w-full
-          sm:w-full
-          lg:w-full
-          text-3xl text-center
-          glass2
-          text-black
-          font-weight-bold
-          mb-5
-          p-3
-        "
-      >
-        Roadmap for Web Development
-      </h1>
-    
-    <div id="roadmapwebdev"
-      class="
-        mx-10
-        my-4
-        glass
-        md:w-1/2 md:h-96
-        lg:h-96
-        sm:w-full
-        lg:w-1/2
-      "
-    >
-      <div
-        class="
-          h-full
-          border-2 border-gray-200 border-opacity-60
-          rounded-lg
-          overflow-hidden
-        "
-      >
-        <iframe
-          width="560"
-          height="315"
-          class="responsive-iframe roundglass p-2"
-          src="https://www.youtube.com/embed/N6Nq62z2C6c"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      </div>
-    </div>
-
-    <div id="linksandresources" 
-    class="mx-10 my-4 glass md:w-1/3">
-      <div
-        class="
-          h-full
-          border-2 border-gray-200 border-opacity-60
-          rounded-lg
-          overflow-hidden
-        "
-      >
-        <div class="p-6">
-          <h2
-            class="
-              tracking-widest
-              text-xs
-              title-font
-              font-medium
-              text-gray-400
-              mb-1
-            "
-          >
-            Roadmaps And Other Resources
-          </h2>
-
-          <h1
-            class="
-              title-font
-              text-lg
-              font-medium
-              text-gray-900
-              mb-3
-            "
-          >
-            Some Useful links
-          </h1>
-          <p class="leading-relaxed mb-3">
-            <a 
-             target="_blank"
-              href="https://docs.google.com/document/d/1jSGO9o5bji8cNcmVfj145wwZ0Kww54IteEvIHIaKJYk/edit?usp=sharing"
-              class="btn btn-primary glass text-black">
-              Complete Web Dev Roadmap </a>
-              <a target="_blank" href="https://www.freecodecamp.org/news/how-the-web-works-a-primer-for-newcomers-to-web-development-or-anyone-really-b4584e63585c/" class="btn btn-primary glass text-black m-2"> How web Works (blog)</a>
-
-            </p>
-         
-           
-          </div>
-        </div>
-
-      </div>
-      <div id="roadmapwebdev"
-      class="
-        mx-10
-        my-4
-        glass
-        md:w-1/2 md:h-96
-        lg:h-96
-        sm:w-full
-        lg:w-1/2
-      "
-    >
-      <div
-        class="
-          h-full
-          border-2 border-gray-200 border-opacity-60
-          rounded-lg
-          overflow-hidden
-        "
-      >
-        <iframe width="560" height="315" class="responsive-iframe roundglass p-2" src="https://www.youtube.com/embed/hJHvdBlSxug" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-      </div>
-    </div>
-    </div>
-  </div>
-
-    
-    `
-    homepagecontainer.removeChild(changingdiv);
-    homepagecontainer.innerHTML = html;
-
-}
-function web3devpage() {
-
-    let html = ` 
-   
-    <div class="flex glass flex-wrap -m-4 my-2" id="webdevcontainer">
     <div class="mx-10 my-4 glass p-2 md:w-1/4" id="frontend">
       <div
         class="
@@ -608,7 +226,78 @@ function web3devpage() {
 
       </div>
     </div>
+    
+    `
+    homepagecontainer.removeChild(changingdiv);
+    homepagecontainer.innerHTML = html;
+
+}
+function web3devpage() {
+
+    let html = ` 
    
+            <div class="flex glass flex-wrap -m-4 my-2" id="web3devcontainer">
+              <div class=" mx-10 my-4 glass md:w-1/4" id="solidity">
+                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="img/website.svg" alt="blog">
+                  <div class="p-6">
+                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+                    <p class="leading-relaxed mb-3"> Solidity smartcontract web 3 dev</p>
+                    <div class="flex items-center flex-wrap ">
+                      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                      <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                          <circle cx="12" cy="12" r="3"></circle>
+                        </svg>1.2K
+                      </span>
+                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                        <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                        </svg>6
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="solana" class=" mx-10 my-4 glass md:w-1/4">
+                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="img/website.svg" alt="blog">
+                  <div class="p-6">
+                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                    <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    <div class="flex items-center flex-wrap">
+                      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                      <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                          <circle cx="12" cy="12" r="3"></circle>
+                        </svg>1.2K
+                      </span>
+                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                        <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                        </svg>6
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+             
+
+                 </div>
 
     
     `
