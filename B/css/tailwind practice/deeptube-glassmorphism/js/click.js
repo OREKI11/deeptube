8da3bd87ada7624b3daf136abb2f50f60ba51b06
Harsh8055 <harshsingh.ytbusiness@gmail.com>
2021-11-01 20:06:40 +0530

    cards added

---
+++ B/css/tailwind practice/deeptube-glassmorphism/js/click.js
@@ -0,0 +1,19 @@
+let profileClick = document.getElementById("profile");
+
+profileClick.addEventListener("click", profileShow);
+function profileShow() {
+
+    let dropdown = document.getElementById("profileclick");
+    let Bool = dropdown.getAttribute("style")
+    if (Bool == "display:none !important"){
+    dropdown.setAttribute("style", "display:inline-block !important") }
+    else {
+        
+        dropdown.setAttribute("style", "display:none !important") 
+
+    }
+
+}
+
+
+
